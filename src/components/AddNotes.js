a405819ea15ed30869ed4d531878776367e92f68
shalinisuryavanshi17 <65484483+shalinisuryavanshi17@users.noreply.github.com>
2021-05-02 20:36:22 +0530

    all the functionalities and styles modified

--- src/components/AddNotes.js
+++ src/components/AddNotes.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 function AddNote(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
+  const [isRefreshed,setRefresh]=useState(false)
   function onInputchange(event) {
     const { name, value } = event.target;
     setNote((prevValue) => {
@@ -13,28 +17,34 @@ function AddNote(props) {
       };
     });
   }
+  function expand()
+  {
+    setRefresh(!isRefreshed)
+  }
   function addNotes(event) {
     event.preventDefault();
     props.addNoteHandler(note);
   }
+  
   return (
-    <form>
+    <form className="create-note">
+   { isRefreshed &&
       <input
         name="title"
         placeholder="Title"
         value={note.title}
         onChange={onInputchange}
-      />
+      />}
       <textarea
         name="content"
         value={note.content}
         onChange={onInputchange}
         placeholder="Take a note..."
-        rows="3"
+        rows={isRefreshed? 3:1} onClick={expand}
       />
-
-      <button onClick={addNotes}>Add</button>
-    </form>
+     <Zoom in={isRefreshed}>
+      <Fab onClick={addNotes}><AddIcon/></Fab></Zoom>
+       </form>
   );
 }
 export default AddNote;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import DeleteIcon from '@material-ui/icons/Delete';
+
+ 
 
 function Note(props) {
   function deleteNote() {
@@ -8,7 +11,7 @@ function Note(props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={deleteNote}>DELETE</button>
+      <button onClick={deleteNote}><DeleteIcon/></button>
     </div>
   );
 }
